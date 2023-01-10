68f368edfae2f72e9d0e8e44185041b86f338f67
oyenet1 <bowofadeoyerinde@gmail.com>
2023-01-10 14:07:42 +0100

    ok done

--- src/App.vue
+++ src/App.vue
@@ -15,12 +15,12 @@ import { RouterLink, RouterView } from "vue-router";
       py-4
     "
   >
+  
+    <RouterView />
     <p class="px-6 mt-12 border rounded-lg py-2 text-sm bg-white shadow-sm">
       Made with <span class="text-purple-500 font-medium">TailwindCss</span> & <span class="text-green-600 font-medium">Vuejs</span> by
       <a href="https://bowofade.com" class="text-blue-600">Bowofade</a>
     </p>
-    <RouterView />
-
     <!--  -->
   </div>
 </template>
